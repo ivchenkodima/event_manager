3983efd30f232596ec7464984ac9d13949062562
ivchenkodima <ivchenkodima@gmail.com>
2017-08-08 01:07:32 +0300

    init

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+
+import Main from './containers/Main';
+import List from './containers/pages/List';
+import AddItem from './containers/pages/AddItem';
+import NotFound from './containers/pages/NotFound';
+
+export const routes = (
+  <div>
+    <Route path='/'>
+      <IndexRoute component={Main} />
+      <Route path='/admin' component={List} />
+      <Route path='/add' component={AddItem} />
+    </Route>
+    <Route path='*' component={NotFound} />
+  </div>
+);
